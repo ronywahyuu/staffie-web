2932d310f6532496945e6f62fee9cf8234ccccc5
ronywahyuu <ronywahyuu@upi.edu>
2022-03-24 15:19:47 +0700

    add salary calculator

--- src/js/index.js
+++ src/js/index.js
@@ -4,23 +4,23 @@
 
 
 const acc1 = {
-    idNumber : 123456,
+    idNumber: 123456,
     name: "Rony Wahyu Utama",
     username: "ronywahyu",
     role: "Manager",
     password: 12345,
     basicSalary: 6000000,
-    allowance : 1500000,
-    cuts : 500000,
+    allowance: 1500000,
+    cuts: 500000,
     // tunjangan 
     workHours: 600,
     attendance: 85,
-    salaryClass : 4,
-    jobPortions : ['Programming','Lead Meeting', 'Code Reviewing'],
-    jobPortionsData : [50,30,15],
+    salaryClass: 4,
+    jobPortions: ['Programming', 'Lead Meeting', 'Code Reviewing'],
+    jobPortionsData: [50, 30, 15],
     dataPerformance: [10, 9, 7, 8, 8, 9],
-    totalSalary : function(){
-        return ((this.basicSalary + this.allowance)-this.cuts);
+    totalSalary: function () {
+        return ((this.basicSalary + this.allowance) - this.cuts);
     },
     workRate: function () {
         const dataSum = this.dataPerformance.reduce((prev, curr) => prev + curr)
@@ -30,23 +30,23 @@ const acc1 = {
 }
 
 const acc2 = {
-    idNumber : 123456,
+    idNumber: 123456,
     name: "Wahyu Utama",
     username: "wahyuu",
     role: "Staff Department",
     password: 54321,
     basicSalary: 4000000,
-    allowance : 1500000,
-    cuts : 500000,
+    allowance: 1500000,
+    cuts: 500000,
     workHours: 500,
     attendance: 85,
     workRate: 9,
-    salaryClass : 3,
-    jobPortions : ['Programming','Lead Meeting', 'Code Reviewing'],
-    jobPortionsData : [50,30,15,5],
+    salaryClass: 3,
+    jobPortions: ['Programming', 'Lead Meeting', 'Code Reviewing'],
+    jobPortionsData: [50, 30, 15, 5],
     dataPerformance: [8, 8, 7, 8, 8, 6],
-    totalSalary : function(){
-        return ((this.basicSalary + this.allowance)-this.cuts);
+    totalSalary: function () {
+        return ((this.basicSalary + this.allowance) - this.cuts);
     },
     workRate: function () {
         const dataSum = this.dataPerformance.reduce((prev, curr) => prev + curr)
@@ -57,23 +57,23 @@ const acc2 = {
 }
 
 const acc3 = {
-    idNumber : 123456,
+    idNumber: 123456,
     name: "Jon Cena",
     username: "joncena",
     role: "Staff Gudang",
     password: 11111,
     basicSalary: 3000000,
-    allowance : 1500000,
-    cuts : 500000,
+    allowance: 1500000,
+    cuts: 500000,
     workHours: 450,
     attendance: 85,
     workRate: 9,
-    salaryClass : 2,
-    jobPortions : ['Programming','Lead Meeting', 'Code Reviewing', 'etc'],
-    jobPortionsData : [50,30,15,5],
+    salaryClass: 2,
+    jobPortions: ['Programming', 'Lead Meeting', 'Code Reviewing', 'etc'],
+    jobPortionsData: [50, 30, 15, 5],
     dataPerformance: [10, 9, 7, 8, 8, 9],
-    totalSalary : function(){
-        return ((this.basicSalary + this.allowance)-this.cuts);
+    totalSalary: function () {
+        return ((this.basicSalary + this.allowance) - this.cuts);
     },
     workRate: function () {
         const dataSum = this.dataPerformance.reduce((prev, curr) => prev + curr)
@@ -138,12 +138,13 @@ class App {
             this.renderPerformanceChart(currentLoggedAcc.dataPerformance)
             this.renderJobTypeChart(currentLoggedAcc.jobPortions, currentLoggedAcc.jobPortionsData)
             this.moreInfoProfile(currentLoggedAcc.idNumber, currentLoggedAcc.salaryClass, currentLoggedAcc.basicSalary, currentLoggedAcc.allowance, currentLoggedAcc.cuts)
-        }else{
+            this.salaryCalculator()
+        } else {
 
             loginFailedAlert()
             console.log('Salah');
         }
-            return currentLoggedAcc.nama;
+        return currentLoggedAcc.nama;
     }
 
     login() {
@@ -169,13 +170,13 @@ class App {
 
     }
 
-    moreInfoProfile(idNumber, salaryClass, BasicSalary, allowance, salaryCuts){
+    moreInfoProfile(idNumber, salaryClass, BasicSalary, allowance, salaryCuts) {
         const btn = document.querySelector('.btn-info')
         btn.addEventListener('click', () => {
             Swal.fire({
                 title: "<strong>Your Info</strong>",
                 // icon: 'info',
-                html : `
+                html: `
                 <table>
                 <tr>
                     <td>
@@ -239,12 +240,134 @@ class App {
                 showCancelButton: true,
                 focusConfirm: false,
                 confirmButtonText:
-                  'Okay',
+                    'Okay',
                 confirmButtonAriaLabel: 'Thumbs up, great!',
                 cancelButtonText:
-                  'Cancel',
+                    'Cancel',
                 cancelButtonAriaLabel: 'Thumbs down'
-              })
+            })
+        })
+    }
+
+    salaryCalculator() {
+        const btnCalculator = document.querySelector('.btn-calculator')
+        const close = document.querySelector('.btn--close-modal')
+        const btn = document.querySelector('.btn-calculate')
+        const name = document.querySelector('.name-calculate')
+        const cuts = Number(document.querySelector('.cuts-calculate').value)
+        const salaryClass = document.querySelector('#salaryClassOptions')
+        const allowance = Number(document.querySelector('.allowance-calculate').value)
+        // return ((this.basicSalary + this.allowance) - this.cuts);
+        const calculate = function(){
+            if(salaryClass.value === 'I'){
+                return 2500000
+            }else if(salaryClass.value === 'II'){
+                return 3000000
+            }else if(salaryClass.value === 'III'){
+                return 4000000
+            }else if(salaryClass.value === 'IV'){
+                return 6000000
+            }else{
+                alert('Invalid Salary')
+            }
+        }
+
+        const total = function(){
+            return (calculate() + allowance) - cuts;  
+        }
+        btn.addEventListener('click', (e) => {
+            e.preventDefault()
+            Swal.fire({
+                title: "<strong>Your Info</strong>",
+                // icon: 'info',
+                html: `
+                <table>
+                <tr>
+                    <td>
+                        <h2>Name</h2>
+                    </td>
+                    <td>
+                        :
+                    </td>
+                    <td>
+                        <h2>${name.value}</h2>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <h2>Salary Class</h2>
+                    </td>
+                    <td>
+                        :
+                    </td>
+                    <td>
+                        <h2>${salaryClass.value}</h2>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <h2>Basic Salary</h2>
+                    </td>
+                    <td>
+                        :
+                    </td>
+                    <td>
+                        <h2>Rp${calculate()}</h2>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <h2>Allowance</h2>
+                    </td>
+                    <td>
+                        :
+                    </td>
+                    <td>
+                        <h2>Rp${allowance}</h2>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <h2>Salary Cuts</h2>
+                    </td>
+                    <td>
+                        :
+                    </td>
+                    <td>
+                        <h2>Rp${cuts}</h2>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <h2>Total Salary</h2>
+                    </td>
+                    <td>
+                        :
+                    </td>
+                    <td>
+                        <h2>Rp${total()}</h2>
+                    </td>
+                </tr>
+                
+            </table>
+                `,
+                showCloseButton: true,
+                showCancelButton: true,
+                focusConfirm: false,
+                confirmButtonText:
+                    'Okay',
+                confirmButtonAriaLabel: 'Thumbs up, great!',
+                cancelButtonText:
+                    'Cancel',
+                cancelButtonAriaLabel: 'Thumbs down'
+            })
+        })
+
+        btnCalculator.addEventListener('click', () => {
+            document.querySelector('.modal').classList.remove('hidden')
+        })
+        close.addEventListener('click', ()=>{
+            document.querySelector('.modal').classList.add('hidden')
         })
     }
 
